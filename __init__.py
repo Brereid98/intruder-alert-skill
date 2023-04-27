8bfdcc5a7fcbbd372ab6569944362689c220f2d4
Brenton Reid <brereid98@gmail.com>
2023-04-27 21:10:52 +1000

    finished script

--- __init__.py
+++ __init__.py
@@ -1,15 +1,25 @@
 from mycroft import MycroftSkill, intent_file_handler
+from mycroft.audio import wait_while_speaking
+from os.path import join
 
-
-class IntruderAlert(MycroftSkill):
+class IntruderAlertSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('alert.intruder.intent')
-    def handle_alert_intruder(self, message):
-        self.speak_dialog('alert.intruder')
+    @intent_file_handler('intruder.alert.intent')
+    def handle_intruder_alert(self, message):
+        # Set the volume to 100%
+        self.enclosure.mouth_text("Increasing volume to 100 percent")
+        self.audioservice.set_volume(100)
 
+        # Play the MP3 file from the desktop
+        self.enclosure.mouth_text("Playing intruder alert sound")
+        mp3_path = '/home/pi/Desktop/intruder_alert.mp3'
+        self.audioservice.play(mp3_path)
+
+        # Wait for the audio to finish playing
+        wait_while_speaking()
 
 def create_skill():
-    return IntruderAlert()
+    return IntruderAlertSkill()
 
